1398bae9930fc726fd57b2ce25fb9963e7c3412f
josh <jjmc.ginley@btinternet.com>
2022-10-18 17:48:44 +0100

    before session 18/10/22

---
+++ src/SumNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        int sumTotal = 0;
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Input a number to sum to: ");
+        int sumTo = reader.nextInt();
+
+        int x = 1;
+        while (x <= sumTo) {
+            sumTotal += x;
+            x++;
+        }
+        System.out.print(sumTotal);
+
+    }
+
+}
